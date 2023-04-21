2bfdff90d2ea5ce931e47cac8683e5ee4cf0e174
samusanc <samusanc@student.42madrid.com>
2023-04-21 20:59:06 +0200

    hola quiero aprobar

---
+++ get_next_line_bonus.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samusanc <samusanc@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/20 17:12:09 by samusanc          #+#    #+#             */
+/*   Updated: 2023/04/21 20:52:17 by samusanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+
+int	ft_verify(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n' || str[i] == '\r')
+			break ;
+		++i;
+	}
+	if (!str[i])
+		return (-1);
+	return (i);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*db[OPEN_MAX];
+	char		*r;
+
+	r = db[fd];
+	if (!r)
+	{
+		r = ft_make_r(r, fd);
+		if (!r)
+			return (NULL);
+		db[fd] = r;
+	}
+	while (ft_verify(db[fd]) == -1)
+	{
+		r = ft_make_r(r, fd);
+		if (!r)
+			return (NULL);
+		r = ft_realloc(db, fd, r, 0);
+		if (!r)
+			return (NULL);
+		db[fd] = r;
+	}
+	r = ft_ch_split(db, fd, 0, 0);
+	if (!r)
+		return (NULL);
+	return (r);
+}
+/*
+int	main()
+{
+	int	fd = open("hola", 0);
+	char *s = "hola";
+
+	while (s)
+	{
+		s = get_next_line(fd);
+		puts(s);
+		free(s);
+	}
+	free(s);
+}
+*/
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samusanc <samusanc@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/20 17:12:09 by samusanc          #+#    #+#             */
+/*   Updated: 2023/04/21 20:52:21 by samusanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+
+char	*ft_make_r(char *r, int fd)
+{
+	r = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!r)
+		return (NULL);
+	r[BUFFER_SIZE] = '\0';
+	if (read(fd, r, BUFFER_SIZE) < 1)
+	{
+		free(r);
+		r = NULL;
+		return (NULL);
+	}
+	return (r);
+}
+
+char	*ft_2d_null(char *str)
+{
+	char	*r;
+	int		i;
+
+	i = 0;
+	if (str[i])
+	{
+		while (str[i])
+			++i;
+		r = malloc(sizeof(char) * (i + 1));
+	}
+	else
+		r = malloc(sizeof(char) * 1);
+	if (!r)
+		return (NULL);
+	r[i] = '\0';
+	i = 0;
+	while (str[i])
+	{
+		r[i] = str[i];
+		++i;
+	}
+	return (r);
+}
+
+char	*ft_ch_split(char **db, int fd, int i, int y)
+{
+	char	*h;
+	char	*t;
+
+	i = ft_verify(db[fd]);
+	t = ft_2d_null(db[fd] + (i + 1));
+	h = malloc(sizeof(char) * (i + 2));
+	if (!h)
+		return (NULL);
+	h[i + 1] = '\0';
+	h[i] = '\n';
+	while (y != i)
+	{
+		h[y] = db[fd][y];
+		++y;
+	}
+	free(db[fd]);
+	db[fd] = NULL;
+	if (t[0])
+		db[fd] = t;
+	else
+	{
+		free(t);
+		db[fd] = NULL;
+	}
+	return (h);
+}
+
+static void	ft_free_naruto(char **db, int fd, char **str)
+{
+	free(db[fd]);
+	db[fd] = NULL;
+	free(*str);
+	*str = NULL;
+}
+
+char	*ft_realloc(char **db, int fd, char *str, int x)
+{
+	char	*r;
+	int		i;
+	int		y;
+
+	i = 0;
+	y = 0;
+	while (db[fd][i])
+		++i;
+	while (str[y])
+		++y;
+	r = malloc(sizeof(char) * (i + y + 1));
+	if (!r)
+		return (NULL);
+	r[i + y] = '\0';
+	while (db[fd][x])
+	{
+		r[x] = db[fd][x];
+		++x;
+	}
+	i = 0;
+	while (i < y)
+		r[x++] = str[i++];
+	ft_free_naruto(db, fd, &str);
+	return (r);
+}
