5995338c22bc43bb6067c5debc66702d4cb02b0c
samusanc <samusanc@student.42madrid.com>
2023-05-17 19:38:24 +0200

    me falta el ch sp

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,36 @@
 /*   By: samusanc <samusanc@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 16:54:20 by samusanc          #+#    #+#             */
-/*   Updated: 2023/05/16 16:57:22 by samusanc         ###   ########.fr       */
+/*   Updated: 2023/05/17 19:34:34 by samusanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include"get_next_line.h"
-// recibe un fd y devuelve la linea leida en forma de string, reservada con malloc
-void	*ft_free(char *str)
+
+void	*ft_free(char **str)
 {
-	if (str)
-		free(str);
+	if (*str)
+	{
+		free(*str);
+		*str = NULL;
+	}
 	return (NULL);
+}
+
+int	ft_rnw_i(char *s, char *db[OPEN_MAX][2], int fd, int i)
+{
+	if (*s == 'R')
+	{
+		if (!db[fd][1])
+			return (0);
+		return ((int)db[fd][1][0]);
+	}
+	if (*s == 'W')
+	{
+		if (!db[fd][1])
+			db[fd][1] = malloc(sizeof(char) * 1);
+		db[fd][1][0] = (char)i;
+	}
+	return (0);
 }
 
 int	ft_verify(char *db[OPEN_MAX][2], int fd)
@@ -25,51 +45,97 @@ int	ft_verify(char *db[OPEN_MAX][2], int fd)
 	i = 0;
 	if (!db[fd][0])
 		return (0);
-	//while (i != (int)db[fd][1])
-	while (db[fd][1][i])
+	while (i != ft_rnw_i("R", db, fd, 0))
 	{
-		if(db[fd][0][i] == '\n' || db[fd][0][i] == '\r')
-		{
+		if (db[fd][0][i] == '\n' || db[fd][0][i] == '\r')
 			return (1);
-		}
 		++i;
 	}
 	return (0);
 }
 
-char	*ft_ch_sp(char *db[OPEN_MAX][2], int fd)
+char	*ft_realloc(char *db[OPEN_MAX][2], int fd, char *s, int i)
 {
 	char	*result;
-	int		i;
 	int		j;
+	int		x;
+	int		y;
 
+	x = 0;
+	y = 0;
 	result = NULL;
-	i = 0;
-	j = 0;
-	while (db[fd][0][i] != '\n' && db[fd][0][i] != '\r')
-		++i;
-	printf("%d", db[fd][0][i]);	
-	result = malloc(sizeof(char) * (i + 1));
+	j = ft_rnw_i("R", db, fd, 0);
+	result = malloc(sizeof(char) * (i + j + 1));
 	if (!result)
-	{
-		//db[fd][0] = ft_free(db[fd][0]);
-		return (0);
-	}
-	result[i] = '\0';
-	while ()
-	{
-	
-	}
-	//db[fd][0] = ft_free(db[fd][0]);
+		return ((char *)ft_free(&db[fd][0]));
+	result[j + i] = '\0';
+	while (x != j)
+		result[x++] = db[fd][0][y++];
+	y = 0;
+	while (x != (i + j))
+		result[x++] = s[y++];
+	ft_free(&db[fd][0]);
+	db[fd][0] = result;
+	ft_rnw_i("W", db, fd, (j + i));
 	return (result);
 }
 
+char	*ft_naruto_ch(char *db[OPEN_MAX][2], int fd, int *i, int x)
+{
+	char	*result;
+	char	*strsave;
+
+	if (!x)
+	{
+		while (db[fd][0][*i] != '\n' && db[fd][0][*i] != '\r')
+			*i = *i + 1;
+		result = malloc(sizeof(char) * (*i + 1));
+		if (!result)
+			return ((char *)ft_free(&db[fd][0]));
+		return (result);
+	}
+	else
+	{
+		strsave = malloc(sizeof(char) * (*i + 1));
+		if (!strsave)
+			return ((char *)ft_free(&db[fd][0]));
+		return (strsave);
+	}
+}
+
+char	*ft_ch_sp(char *db[OPEN_MAX][2], int fd, int i, int j)
+{
+	char	*result;
+	char	*strsave;
+	int		k;
+
+	result = ft_naruto_ch(db, fd, &i, 0);
+	if (!result)
+		return (0);
+	result[i] = '\0';
+	while (j <= i)
+		result[j++] = db[fd][0][k++];
+	i = 0;
+	while (k++ != ft_rnw_i("R", db, fd, 0))
+		++i;
+	strsave = ft_naruto_ch(db, fd, &i, 1);
+	if (!strsave)
+		return (0);
+	strsave[i] = '\0';
+	k = 0;
+	while (k != i)
+		strsave[k++] = db[fd][0][j++];
+	ft_rnw_i("W", db, fd, k);
+	db[fd][0] = ft_free(&db[fd][0]);
+	db[fd][0] = strsave;
+	return (result);
+}
 char	*get_next_line(int fd)
 {
 	static char	*db[OPEN_MAX][2];
-	char	s[BUFFER_SIZE + 1];
-	char	*result;
-	int	i;
+	char		s[BUFFER_SIZE + 1];
+	char		*result;
+	int			i;
 
 	i = 1;
 	result = NULL;
@@ -77,21 +143,22 @@ char	*get_next_line(int fd)
 	{
 		if (ft_verify(db, fd))
 		{
-			return (ft_ch_sp(db, fd));
+			break ;
 		}
+		//	return (ft_ch_sp(db, fd, 0, 0));
 		i = read(fd, s, BUFFER_SIZE);
 		if (i < 0)
-		{
-			db[fd][0] = (char *)ft_free(db[fd][0]);
-			return (0);
-		}
+			return ((char *)ft_free(&db[fd][0]));
 		if (!i)
 			break ;
 		s[i] = '\0';
-		//db[fd][0] = ft_realloc(db, fd, s, i);
+		if (!ft_realloc(db, fd, s, i))
+			return (0);
 	}
+	//ft_ch_sp(db, fd, 0, 0);
+	printf("%s", db[fd][0]);
 	//result = ft_memcpy(db[fd][0], db[fd][1]);
-	db[fd][0] = (char *)ft_free(db[fd][0]);
+	db[fd][0] = (char *)ft_free(&db[fd][0]);
 	return (result);
 }
 
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: samusanc <samusanc@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 16:54:20 by samusanc          #+#    #+#             */
-/*   Updated: 2023/05/16 16:57:29 by samusanc         ###   ########.fr       */
+/*   Updated: 2023/05/17 19:34:39 by samusanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -26,15 +26,19 @@ int	main()
 		s = get_next_line(fd);
 	}
 	*/
-	/*
+	
 	int	fd;
 
 	fd = open ("get_next_line.c", O_RDONLY);
-	free(get_next_line(fd));
-	*/
+	printf("%s", get_next_line(fd));
+	
+	/*
 	char	*db[OPEN_MAX][2];
-	char	*s = "ho\nla";
+	char	*s = "hola";
+	char	*str = "adeu";
 	db[1][0] = s;
-	ft_ch_sp(db, 1);
+	ft_rnw_i("W", db, 1, 4);
+	ft_realloc(db, 1, NULL, 0);
+	*/
 	system("leaks -q aoeu");
 }
